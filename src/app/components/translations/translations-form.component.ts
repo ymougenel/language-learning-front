6e107a6cf3b37b08d78088e140a16599eeb3e401
Yann Mougenel <yann.mougenel@gmail.com>
2020-11-12 18:15:12 +0100

    feat: fist working version, let's eat

--- src/app/components/translations/translations-form.component.ts
+++ src/app/components/translations/translations-form.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Translation} from "../../core/models/translations";
-import {Translations} from "../../core/models/mock/translation-mock";
 import {TranslationsService} from "../../core/services/translations/translations.service";
 
 @Component({
@@ -11,11 +10,44 @@ import {TranslationsService} from "../../core/services/translations/translations
 export class TranslationsFormComponent implements OnInit {
 
 
-  constructor(private translationsService: TranslationsService) { }
-
-  public translations: Translation[];
-  ngOnInit() {
-    this.translations = Translations;
+  constructor(private translationsService: TranslationsService) {
   }
 
+  private translationCount = 10;
+
+  public translations: Translation[];
+  public answers: Translation[] = [];
+  public correctionMode = false;
+
+
+  ngOnInit() {
+    this.getTranslations();
+    this.hideTranslation();
+  }
+
+  getTranslations() {
+    this.translations = this.translationsService.getTranslations(this.translationCount);
+    this.answers = [];
+    this.translations.forEach(val => this.answers.push(Object.assign({}, val)));
+  }
+
+  hideTranslation() {
+    for (let translation of this.translations) {
+      let rand = Math.floor(Math.random() * 2);
+      if (rand) {
+        translation.word1="";
+        translation.word2=null;
+      }
+      else {
+        translation.word1=null;
+        translation.word2="";
+      }
+    }
+  }
+
+  validateTranslations() {
+    console.log(this.translations[0]);
+    console.log(this.answers[0]);
+    this.correctionMode=true;
+  }
 }
--- src/app/core/services/translations/translations.service.ts
+++ src/app/core/services/translations/translations.service.ts
@@ -1,15 +1,16 @@
 import {Injectable} from '@angular/core';
 import {HttpClientConfig} from "../http-client-config";
 import {Observable} from "rxjs";
+import {Translations} from "../../models/mock/translation-mock";
+import {Translation} from "../../models/translations";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class TranslationsService {
 
   constructor(private readonly _config: HttpClientConfig) {
   }
 
-  getTranslations(count: number) : string {
-    // return null;
-    return this._config.baseUrl;
+  getTranslations(count: number): Translation[] {
+    return Translations.sort(() => 0.5 - Math.random()).slice(0, count);
   }
 }
